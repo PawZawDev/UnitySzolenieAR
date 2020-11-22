6e63e22d840d9c6d89749a6452565f1db0c5f607
PawZaw <49035966+PawZawDev@users.noreply.github.com>
2020-11-22 18:52:23 +0100

    Add continue

--- Assets/Components/Marker/MarkerScript.cs
+++ Assets/Components/Marker/MarkerScript.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -6,30 +7,33 @@ using UnityEngine.UI;
 public class MarkerScript : MonoBehaviour
 {
     public GameObject markerCanvasObject;
-    public Button yesButton;
+    public string MarkerObjectName;
+    public Button YesButton;
+    public Button NoButton;
+    public Button ContinueButton;
     public Text questionText;
 
     public void OnYesButtonClick()
     {
         questionText.text = "ahh yes bulbulator indeed is a tool to bulbulate";
     }
+    public void OnContinueButtonClick()
+    {
+        ContinueButton.gameObject.SetActive(false);
+        YesButton.gameObject.SetActive(true);
+        NoButton.gameObject.SetActive(true);
+        questionText.text = "Does the bulbator bulbate?";
+    }
 
-    //void onLook()
-    //{
-    //    markerCanvasObject.SetActive(true);
-    //}
-    
-    //void noLook()
-    //{
-    //    markerCanvasObject.SetActive(false);
-    //}
-    
     // Start is called before the first frame update
     void Start()
     {
+        markerCanvasObject.transform.Find("MarkerObjectName").GetComponent<Text>().text = MarkerObjectName;
         markerCanvasObject.SetActive(false);
         markerCanvasObject.transform.localScale = new Vector3(-markerCanvasObject.transform.localScale.x, markerCanvasObject.transform.localScale.y, markerCanvasObject.transform.localScale.z);
-        yesButton.onClick.AddListener(OnYesButtonClick);
+        YesButton.onClick.AddListener(OnYesButtonClick);
+        ContinueButton.onClick.AddListener(OnContinueButtonClick);
+        ContinueButton.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
@@ -42,21 +46,18 @@ public class MarkerScript : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         markerCanvasObject.SetActive(true);
+        StartCoroutine("CountTimeToContinue");
+    }
+
+    private IEnumerator CountTimeToContinue()
+    {
+        yield return new WaitForSeconds(10);
+        ContinueButton.gameObject.SetActive(true);
     }
 
     private void OnTriggerExit(Collider other)
     {
         markerCanvasObject.SetActive(false);
+        StopCoroutine("CountTimeToContinue");
     }
-    private void OnCollisionEnter(Collision collision)
-    {
-        markerCanvasObject.SetActive(true);
-    }
-
-    private void OnCollisionExit(Collision collision)
-    {
-        markerCanvasObject.SetActive(false);
-    }
-
-
 }
