cce485d3ccc6bc5a57203b82f5cb3fd605154878
SchopenXD <32485285+SchopenXD@users.noreply.github.com>
2020-11-25 17:33:01 +0100

    Camera slave
    Initial commit xd

---
+++ Assets/Scripts/DrawFigure.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class DrawFigure : MonoBehaviour
+{
+    [DllImport("OCVWrapper", EntryPoint = "detectFace", CallingConvention = CallingConvention.StdCall)]
+    public static extern void detectFace(long length, int width, int height, byte[] data, out float tlx, out float tly, out float brx, out float bry);
+    [DllImport("OCVWrapper", EntryPoint = "loadClassifier", CallingConvention = CallingConvention.StdCall)]
+    public static extern void loadClassifier();
+
+
+    new private Renderer renderer;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        renderer = GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Texture2D t = new Texture2D(renderer.material.mainTexture.width, renderer.material.mainTexture.height, TextureFormat.RGB24, false);
+        t.SetPixels((renderer.material.mainTexture as WebCamTexture).GetPixels());
+        t.Apply();
+        byte[] bytes = t.GetRawTextureData();
+        float tlx = 0, tly = 0, brx = 0, bry = 0;
+        detectFace(bytes.Length, t.width, t.height, bytes, out tlx, out tly, out brx, out bry);
+        Debug.Log("FaceCoords:(" + tlx + ", " + tly + ", " + brx + ", " + bry + ",) ");
+        renderer.sharedMaterial.SetFloat("CX", 1 - (tlx + brx / 2) / t.width);
+        renderer.sharedMaterial.SetFloat("CY", (tly + bry / 2) / t.height);
+        renderer.sharedMaterial.SetFloat("R", bry / (2 * t.height));
+    }
+}
---
+++ Assets/Scripts/PhysicalCameraStreamer.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.IO;
+using UnityEngine;
+using System;
+
+public class PhysicalCameraStreamer : MonoBehaviour
+{
+    public Texture2D tex;
+    public GameObject ResultView;
+    new public Renderer renderer;
+
+    private WebCamTexture webcamTexture;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        webcamTexture = new WebCamTexture();
+        renderer = GetComponent<Renderer>();
+        renderer.material.mainTexture = webcamTexture;
+
+    }
+
+    public void Play() => webcamTexture.Play();
+    public void Pause() => webcamTexture.Pause();
+    public void Stop() => webcamTexture.Stop();
+
+    private void Update()
+    {
+        if (webcamTexture.isPlaying)
+        {
+
+        }
+    }
+}
